e2981ee8c8daf126fd768f57c1dfd3faabf4842e
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2026-05-29 14:05:20 -0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) == 1:
+            return True
+        l = 0
+        r = len(s) - 1
+
+        while r > l:
+            if not s[l].isalnum():
+                l += 1
+                continue
+            if not s[r].isalnum():
+                r -= 1
+                continue
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+
+        return True
