45389b87de187dece1e60b2f5ce83519041b3565
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2026-06-02 12:41:07 -0300

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        count = 0
+
+        while len(nums[l:r+1]) > 1:
+            mid = (l + r) // 2
+            if nums[mid] > target:
+                r = mid - 1
+                continue
+            elif nums[mid] < target:
+                l = mid + 1
+                continue
+            else: #if equal to mid
+                return mid
+        if nums[l] == target:
+            return l
+
+        return -1
